28d523c87156fceb48725bd74b411a683a4e36f1
Ankush Choudhary <ankush282000@gmail.com>
2025-01-29 18:06:08 +0530

    Refactor Lambda and S3 constructs to use 'Construct' from 'constructs' module

--- infra/constructs/lambda_construct.py
+++ infra/constructs/lambda_construct.py
@@ -1,12 +1,12 @@
-from aws_cdk as core
+
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_lambda as lambda_
 from aws_cdk import aws_events as events
 from aws_cdk import aws_events_targets as targets
 from aws_cdk.aws_iam import Role, ServicePrincipal, ManagedPolicy
-
-class LambdaConstruct(core.Construct):
-    def __init__(self, scope: core.Construct, id: str, 
+from constructs import Construct
+class LambdaConstruct(Construct):
+    def __init__(self, scope: Construct, id: str, 
                  function_name: str,
                  code_path: str,
                  handler: str,
--- infra/constructs/s3_construct.py
+++ infra/constructs/s3_construct.py
@@ -1,9 +1,9 @@
 from aws_cdk import (
     aws_s3 as s3
 )
-
-class S3Construct(core.Construct):
-    def __init__(self, scope: core.Construct, id: str, bucket_name: str, **kwargs):
+from constructs import Construct
+class S3Construct(Construct):
+    def __init__(self, scope: Construct, id: str, bucket_name: str, **kwargs):
         super().__init__(scope, id, **kwargs)
         
         self.bucket = s3.Bucket(
